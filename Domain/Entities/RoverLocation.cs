b483bada69e9a09ad6e994ba1579f0a09365dd0c
Do1ore <100031663+Do1ore@users.noreply.github.com>
2023-08-19 19:01:30 +0300

    +RobotRover.cs multiple instance  bug fixed

---
+++ Domain/Entities/RoverLocation.cs
@@ -0,0 +1,27 @@
+﻿using Domain.DTOs;
+using Domain.Enums;
+using System.ComponentModel.DataAnnotations;
+
+namespace Domain.Entities;
+
+public class RoverLocation
+{
+    [Key]
+    public int Id { get; set; }
+    public long XPosition { get; set; }
+    public long YPosition { get; set; }
+    public bool IsAlive { get; set; }
+    [EnumDataType(typeof(Direction))] public string? CurrentDirection { get; set; }
+    public DateTime LastLocationDateTime { get; set; }
+
+    public static implicit operator RoverLocation(RoverLocationDto roverLocation)
+    {
+        return new RoverLocation
+        {
+            XPosition = roverLocation.XPosition,
+            YPosition = roverLocation.YPosition,
+            CurrentDirection = roverLocation.CurrentDirection,
+            LastLocationDateTime = roverLocation.LastLocationDateTime,
+        };
+    }
+}
